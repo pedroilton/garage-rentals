fac5104cad42ff5c5b38a29c1a7af47e34e038be
Pedro Ilton <pedroilton@gmail.com>
2020-12-03 15:54:06 -0300

    Starting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  # root to: 'pages#home'
+  root to: 'garages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :garages do
+    resources :rentals, only: %i[new create]
+    resources :reviews, only: %i[new create]
+  end
+
+  resources :users do
+  end
 end
