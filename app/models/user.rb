61180b29333d51d860e4956b1fb00dabf504a8e4
Eduardo Ritter <eduardoritter@hotmail.com>
2020-12-03 18:50:50 -0300

    destroy_dependent included in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :rentals
-  has_many :garages
+  has_many :rentals, dependent: :destroy
+  has_many :garages, dependent: :destroy
 
   validates :name, presence: true
 end
