b13a6375c8c99b15a58c22e766c5fd8b414f6e03
Eduardo Ritter <eduardoritter@hotmail.com>
2020-12-03 16:39:39 -0300

    gitignore ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'
+  # root to: 'pages#home'
+  root to: 'garages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :garages do
+    resources :rentals, only: %i[new create]
+  end
+
+  resources :rentals, only: %i[index edit update] do
+    resources :reviews, only: %i[new create]
+  end
+
+  get 'users/:id/garages', to: 'garages#list'
 end
