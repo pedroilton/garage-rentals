58f1e69924479e6ee0c9451c351cecdadf9f91f9
Pedro Ilton <pedroilton@gmail.com>
2020-12-04 13:46:19 -0300

    Devise running and rentals controller and views drafts

--- app/controllers/garages_controller.rb
+++ app/controllers/garages_controller.rb
@@ -1,4 +1,5 @@
 class GaragesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
   before_action :set_garage, only: %i[show edit update destroy]
 
   def index
