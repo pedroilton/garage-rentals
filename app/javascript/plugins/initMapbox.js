c40a7d1c255858a0909dc15824ee5c00dd5a7476
Pedro Ilton <pedroilton@gmail.com>
2020-12-07 19:52:29 -0300

    Starting with current location

--- app/javascript/plugins/initMapbox.js
+++ app/javascript/plugins/initMapbox.js
@@ -12,19 +12,38 @@ const initMapbox = () => {
 
   if (mapElement) { // only build a map if there's a div#map to inject into
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
-    });
-    // Adicionar marcadores:
-    const markers = JSON.parse(mapElement.dataset.markers);
-    markers.forEach((marker) => {
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
-        .addTo(map);
-    });
-    // Chama o fitMapToMarkers:
-    fitMapToMarkers(map, markers);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((coord) => {
+        const map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/mapbox/streets-v11',
+          zoom: 11,
+          center: [coord.coords.longitude, coord.coords.latitude]
+        });
+        // Adicionar marcadores:
+        const markers = JSON.parse(mapElement.dataset.markers);
+        markers.forEach((marker) => {
+          new mapboxgl.Marker()
+            .setLngLat([ marker.lng, marker.lat ])
+            .addTo(map);
+        });
+      });
+    }
+    else {
+      const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11'
+      });
+      // Adicionar marcadores:
+      const markers = JSON.parse(mapElement.dataset.markers);
+      markers.forEach((marker) => {
+        new mapboxgl.Marker()
+          .setLngLat([ marker.lng, marker.lat ])
+          .addTo(map);
+      });
+      // Chama o fitMapToMarkers:
+      fitMapToMarkers(map, markers);
+    }
   }
 };
 
